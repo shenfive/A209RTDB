b8c96f9b9b497db8f59b28070f9f8faf9654fcd6
shenfive@gmail.com <Danny Shen>
2022-11-05 16:12:12 +0800

    read Data

--- A209RTDB/Page3ViewController.swift
+++ A209RTDB/Page3ViewController.swift
@@ -10,17 +10,46 @@ import Firebase
 
 class Page3ViewController: UIViewController {
 
+    @IBOutlet weak var theTableview: UITableView!
     var subject:[String:String] = [:]
     var nickname = ""
     @IBOutlet weak var inputText: UITextField!
+    
+    var list:[[String:Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print(subject)
         print(nickname)
-        
+        theTableview.dataSource = self
         self.title = subject["subject"]
+        
+        
+        let key = subject["key"] ?? ""
+        
+        ref.child("d").child(key).observe(.value) { snapshot in
+            for item in snapshot.children{
+                let s1 = item as! DataSnapshot
+                print(s1)
+                print()
+                let sitem = ["content":s1.childSnapshot(forPath: "c").value as! String,
+                             "name":s1.childSnapshot(forPath: "n").value as! String,
+                             "t":s1.childSnapshot(forPath: "t").value as! Double]
+                self.list.append(sitem)
+                
+            }
+            print(self.list)
+            print(self.list.count)
+            print()
+        
+            
+         
+
+        }
+        
+        
+        
 
     }
     
@@ -46,5 +75,17 @@ class Page3ViewController: UIViewController {
         
         
     }
+    
+}
+
+extension Page3ViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     
 }
