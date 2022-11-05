38f54c3be39ccc6a4c0e22f892a09c89867c0d89
shenfive@gmail.com <Danny Shen>
2022-11-05 13:35:00 +0800

    Page2 TableView

--- A209RTDB/Page2ViewController.swift
+++ A209RTDB/Page2ViewController.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class Page2ViewController: UIViewController {
 
+    @IBOutlet weak var theTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.title = "討論區列表"
+        
+        theTableView.delegate = self
+        theTableView.dataSource = self
+    }
+}
+
+extension Page2ViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
-
-  
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.backgroundColor = UIColor.yellow
+        
+        return cell
+    }
+    
+    
 }
